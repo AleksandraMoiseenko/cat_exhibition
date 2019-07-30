56afaf43ed3635c2ff2e56ee1b0635d13635921e
AleksandraMoiseenko <53462962+AleksandraMoiseenko@users.noreply.github.com>
2019-07-30 08:59:35 +0300

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,362 @@
+package cat_exhibition;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Cat {
+	private String name;
+	private String food;
+	private String breed;
+	private HashSet<Human> owners;
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public HashSet<Human> getOwners() {
+		return owners;
+	}
+
+	public void setOwners(HashSet<Human> owners) {
+		this.owners = owners;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printfood() {
+		System.out.println("Cat's food: " + this.getFood());
+	}
+
+	public void printbreed() {
+		System.out.println("Cat's breed: " + this.getBreed());
+	}
+
+	public void printname() {
+		System.out.println("Cat's name: " + this.getName());
+	}
+
+	public void printowner() {
+		for (Human owner : this.getOwners()) {
+			owner.printhumaninfo();
+		}
+	}
+
+	public static void writeUsingOutputStream(String info) throws IOException {
+		FileOutputStream fileOutputStream = new FileOutputStream(
+				"/Users/svetlanamoiseenko/Desktop/cat_exhibition_output.txt", true);
+		String lineSeparator = System.getProperty("line.separator");
+		fileOutputStream.write(info.getBytes());
+		fileOutputStream.write(lineSeparator.getBytes());
+		fileOutputStream.flush();
+		fileOutputStream.close();
+	}
+
+	public static void writeUsingInputStream() throws IOException {
+		FileInputStream fileInputStream = new FileInputStream(
+				"/Users/svetlanamoiseenko/Desktop/cat_exhibition_input.txt");
+
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 200);
+
+		int i;
+
+		while ((i = bufferedInputStream.read()) != -1)
+
+			System.out.print((char) i);
+
+	}
+
+	public void printcatinfo() {
+		System.out.println("Cat's name :" + this.getName());
+		System.out.println("Cat's breed :" + this.getBreed());
+		System.out.println("Cat's food :" + this.getFood());
+		System.out.println("Cat's owners: ");
+		for (Human owner : owners) {
+			owner.printhumaninfo();
+		}
+
+	}
+
+	public static void searchbyid(HashMap<Integer, Cat> catlist, int id) throws IOException {// search cat by cat
+																								// exhibition number
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getKey() == id) {
+				cat.getValue().printcatinfo();
+				writeUsingOutputStream(cat.getValue().getName());
+				writeUsingOutputStream(cat.getValue().getBreed());
+				writeUsingOutputStream(cat.getValue().getFood());
+				for (Human owner : cat.getValue().getOwners()) {
+					writeUsingOutputStream(owner.getName());
+					writeUsingOutputStream(owner.getLast_name());
+					writeUsingOutputStream(owner.getAddress());
+				}
+			}
+		}
+	}
+
+	public static void searchbyname(HashMap<Integer, Cat> catlist, String name) throws IOException {// search cat info
+																									// by it's name
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getValue().getName().equals(name)) {
+				System.out.println("Exhibition number: " + cat.getKey());
+				cat.getValue().printcatinfo();
+				writeUsingOutputStream("Exhibition number: " + cat.getKey());
+				writeUsingOutputStream(cat.getValue().getName());
+				writeUsingOutputStream(cat.getValue().getBreed());
+				writeUsingOutputStream(cat.getValue().getFood());
+				for (Human owner : cat.getValue().getOwners()) {
+					writeUsingOutputStream(owner.getName());
+					writeUsingOutputStream(owner.getLast_name());
+					writeUsingOutputStream(owner.getAddress());
+				}
+			}
+		}
+	}
+
+	public static void searchbylast_name(HashMap<Integer, Cat> catlist, HashSet<Human> owners) throws IOException {// search
+																													// cat
+																													// by
+																													// it's
+																													// list
+																													// of
+																													// owners
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getValue().getOwners().equals(owners)) {
+				System.out.println("Exhibition number: " + cat.getKey());
+				cat.getValue().printcatinfo();
+				writeUsingOutputStream("Exhibition number: " + cat.getKey());
+				writeUsingOutputStream(cat.getValue().getName());
+				writeUsingOutputStream(cat.getValue().getBreed());
+				writeUsingOutputStream(cat.getValue().getFood());
+				for (Human owner : cat.getValue().getOwners()) {
+					writeUsingOutputStream(owner.getName());
+					writeUsingOutputStream(owner.getLast_name());
+					writeUsingOutputStream(owner.getAddress());
+				}
+			}
+		}
+	}
+
+	public static void searchbybreed(HashMap<Integer, Cat> catlist, String breed) throws IOException {// search cat info
+																										// by breed
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getValue().getBreed().equals(breed)) {
+				System.out.println("Exhibition number: " + cat.getKey());
+				cat.getValue().printcatinfo();
+				writeUsingOutputStream("Exhibition number: " + cat.getKey());
+				writeUsingOutputStream(cat.getValue().getName());
+				writeUsingOutputStream(cat.getValue().getBreed());
+				writeUsingOutputStream(cat.getValue().getFood());
+				for (Human owner : cat.getValue().getOwners()) {
+					writeUsingOutputStream(owner.getName());
+					writeUsingOutputStream(owner.getLast_name());
+					writeUsingOutputStream(owner.getAddress());
+				}
+			}
+		}
+	}
+
+	public static void searchbyfood(HashMap<Integer, Cat> catlist, String food) throws IOException {// search cat info
+																									// by food
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getValue().getFood().equals(food)) {
+				System.out.println("Exhibition number: " + cat.getKey());
+				cat.getValue().printcatinfo();
+				writeUsingOutputStream("Exhibition number: " + cat.getKey());
+				writeUsingOutputStream(cat.getValue().getName());
+				writeUsingOutputStream(cat.getValue().getBreed());
+				writeUsingOutputStream(cat.getValue().getFood());
+				for (Human owner : cat.getValue().getOwners()) {
+					writeUsingOutputStream(owner.getName());
+					writeUsingOutputStream(owner.getLast_name());
+					writeUsingOutputStream(owner.getAddress());
+				}
+			}
+		}
+	}
+
+	public static HashMap<Integer, Cat> updateCatName(HashMap<Integer, Cat> catlist, int id, String newCatName)
+			throws IOException {// update
+		// cat
+		// name
+		// by
+		// id
+		HashMap<Integer, Cat> updateCatList = new HashMap<Integer, Cat>();
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getKey() == id)
+				cat.getValue().setName(newCatName);
+		}
+		updateCatList.putAll(catlist);
+		for (Map.Entry<Integer, Cat> cat : updateCatList.entrySet()) {
+			System.out.println("Exhibition number: " + cat.getKey());
+			cat.getValue().printcatinfo();
+			writeUsingOutputStream("Exhibition number: " + cat.getKey());
+			writeUsingOutputStream(cat.getValue().getName());
+			writeUsingOutputStream(cat.getValue().getBreed());
+			writeUsingOutputStream(cat.getValue().getFood());
+			for (Human owner : cat.getValue().getOwners()) {
+				writeUsingOutputStream(owner.getName());
+				writeUsingOutputStream(owner.getLast_name());
+				writeUsingOutputStream(owner.getAddress());
+			}
+		}
+		return updateCatList;
+	}
+
+	public static HashMap<Integer, Cat> updateCatFood(HashMap<Integer, Cat> catlist, int id, String newCatFood)//update cat food by id
+			throws IOException {
+		HashMap<Integer, Cat> updateCatList = new HashMap<Integer, Cat>();
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			if (cat.getKey() == id)
+				cat.getValue().setFood(newCatFood);
+
+		}
+		updateCatList.putAll(catlist);
+		for (Map.Entry<Integer, Cat> cat : updateCatList.entrySet()) {
+			System.out.println("Exhibition number: " + cat.getKey());
+			cat.getValue().printcatinfo();
+			writeUsingOutputStream("Exhibition number: " + cat.getKey());
+			writeUsingOutputStream(cat.getValue().getName());
+			writeUsingOutputStream(cat.getValue().getBreed());
+			writeUsingOutputStream(cat.getValue().getFood());
+			for (Human owner : cat.getValue().getOwners()) {
+				writeUsingOutputStream(owner.getName());
+				writeUsingOutputStream(owner.getLast_name());
+				writeUsingOutputStream(owner.getAddress());
+			}
+		}
+		return updateCatList;
+	}
+
+	/*
+	 * public static HashMap<Integer, Cat> updateCatId(HashMap<Integer, Cat>
+	 * catlist, int old_id, int new_id) throws IOException { HashMap<Integer, Cat>
+	 * updateCatList = new HashMap<Integer, Cat>(); if(catlist.containsKey(new_id))
+	 * { System.out.
+	 * println("Cat with this id has already exist. Please change new id number.");
+	 * }else { Cat buffer=null; for (Map.Entry<Integer, Cat> cat :
+	 * catlist.entrySet()) { if(cat.getKey()==old_id) buffer=new
+	 * Cat(cat.getValue().getName(),cat.getValue().getFood(),cat.getValue().getBreed
+	 * (),cat.getValue().getOwners());
+	 * 
+	 * updateCatList.put(new_id, buffer); } catlist.remove(old_id);
+	 * 
+	 * updateCatList.putAll(catlist); for (Map.Entry<Integer, Cat> cat :
+	 * updateCatList.entrySet()) { System.out.println("Exhibition number: " +
+	 * cat.getKey()); cat.getValue().printcatinfo();
+	 * writeUsingOutputStream("Exhibition number: " + cat.getKey());
+	 * writeUsingOutputStream(cat.getValue().getName());
+	 * writeUsingOutputStream(cat.getValue().getBreed());
+	 * writeUsingOutputStream(cat.getValue().getFood()); for (Human owner :
+	 * cat.getValue().getOwners()) { writeUsingOutputStream(owner.getName());
+	 * writeUsingOutputStream(owner.getLast_name());
+	 * writeUsingOutputStream(owner.getAddress()); } } //return updateCatList; }
+	 * return updateCatList; }
+	 */
+	public static HashMap<Integer, Cat> updateCatOwnersName(HashMap<Integer, Cat> catlist, int id,
+			HashSet<Human> owners, String last_name, String name) throws IOException {// update cat owners by id(if cat was bought after the exhibition)
+		HashMap<Integer, Cat> updateCatList = new HashMap<Integer, Cat>();
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			for (Human owner : owners) {
+				if (cat.getKey() == id)
+					owner.setName(name);
+				owner.setLast_name(last_name);
+			}
+		}
+		updateCatList.putAll(catlist);
+		for (Map.Entry<Integer, Cat> cat : updateCatList.entrySet()) {
+			System.out.println("Exhibition number: " + cat.getKey());
+			cat.getValue().printcatinfo();
+			writeUsingOutputStream("Exhibition number: " + cat.getKey());
+			writeUsingOutputStream(cat.getValue().getName());
+			writeUsingOutputStream(cat.getValue().getBreed());
+			writeUsingOutputStream(cat.getValue().getFood());
+			for (Human owner : cat.getValue().getOwners()) {
+				writeUsingOutputStream(owner.getName());
+				writeUsingOutputStream(owner.getLast_name());
+				writeUsingOutputStream(owner.getAddress());
+			}
+		}
+		return updateCatList;
+	}
+
+	public static HashMap<Integer,Cat> updateCatAddress(HashMap<Integer, Cat> catlist, int id, String newAddress)
+			throws IOException {// update
+		// cat
+		// address
+		// by
+		// cat's
+		// id
+		HashMap<Integer, Cat> updateCatList = new HashMap<Integer, Cat>();
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			for (Human owner : cat.getValue().getOwners()) {
+				if (cat.getKey() == id)
+					owner.setAddress(newAddress);
+			}
+		}
+		updateCatList.putAll(catlist);
+		
+		for (Map.Entry<Integer, Cat> cat : updateCatList.entrySet()) {
+			System.out.println("Exhibition number: " + cat.getKey());
+			cat.getValue().printcatinfo();
+			writeUsingOutputStream("Exhibition number: " + cat.getKey());
+			writeUsingOutputStream(cat.getValue().getName());
+			writeUsingOutputStream(cat.getValue().getBreed());
+			writeUsingOutputStream(cat.getValue().getFood());
+			for (Human owner : cat.getValue().getOwners()) {
+				writeUsingOutputStream(owner.getName());
+				writeUsingOutputStream(owner.getLast_name());
+				writeUsingOutputStream(owner.getAddress());
+			}
+		}
+		return updateCatList;
+	}
+
+	public static void deletecat(HashMap<Integer, Cat> catlist, int id) throws IOException {//delete cat from list
+		catlist.remove(id);
+		for (Map.Entry<Integer, Cat> cat : catlist.entrySet()) {
+			System.out.println("Exhibition number: " + cat.getKey());
+			cat.getValue().printcatinfo();
+			writeUsingOutputStream("Exhibition number: " + cat.getKey());
+			writeUsingOutputStream(cat.getValue().getName());
+			writeUsingOutputStream(cat.getValue().getBreed());
+			writeUsingOutputStream(cat.getValue().getFood());
+			for (Human owner : cat.getValue().getOwners()) {
+				writeUsingOutputStream(owner.getName());
+				writeUsingOutputStream(owner.getLast_name());
+				writeUsingOutputStream(owner.getAddress());
+			}
+		}
+	}
+
+	public Cat(String name, String food, String breed, HashSet<Human> owners) {
+		this.name = name;
+		this.food = food;
+		this.breed = breed;
+		this.owners = owners;
+
+	}
+
+}
---
+++ Check.java
@@ -0,0 +1,52 @@
+package cat_exhibition;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Check {
+
+	public static void main(String[] args) throws IOException {
+		HashSet<Human> Kolesnikov = new HashSet<Human>();
+		Kolesnikov.add(new Human("Kolesnikov", "Semen", "Moscow"));
+		HashSet<Human> Cherepanov = new HashSet<Human>();
+		Cherepanov.add(new Human("Alexey", "Cherepanov", "Moscow"));
+		Cherepanov.add(new Human("Vika", "Cherepanova", "Moscow"));
+		HashSet<Human> Levina = new HashSet<Human>();
+		Levina.add(new Human("Levina", "Natasha", "NN"));
+		HashSet<Human>Abramov=new HashSet<Human>();
+		Abramov.add(new Human("Alisa","Abramova","Kalinigrad"));
+		Abramov.add(new Human("Elena","Abramova","Kaliningrad"));
+		HashSet<Human>Viktorov=new HashSet<Human>();
+		Viktorov.add(new Human("Oleg","Viktorov","Saratov"));
+		HashMap<Integer, Cat> cat_exh = new HashMap<Integer, Cat>();
+		cat_exh.put(1, new Cat("Murphy", "purina", "siam", Cherepanov));
+		cat_exh.put(2, new Cat("John", "proplan", "bengal", Kolesnikov));
+		cat_exh.put(3, new Cat("Fluffy", "proplan", "wood cat", Levina));
+		cat_exh.put(4,new Cat("Choco","proplan","australian mist",Abramov));
+		cat_exh.put(5,new Cat("Mia","royal canin","abissin cat",Viktorov));
+		for (Map.Entry<Integer, Cat> cat : cat_exh.entrySet()) {
+			cat.getValue().printcatinfo();
+		}
+		Cat.searchbyid(cat_exh, 1);
+		Cat.searchbyname(cat_exh, "John");
+		Cat.searchbylast_name(cat_exh, Kolesnikov);
+		Cat.searchbybreed(cat_exh, "siam");
+		Cat.searchbyfood(cat_exh, "purina");
+		Cat.searchbybreed(cat_exh, "bengal");
+		Cat.updateCatName(cat_exh, 1, "Alex");
+		Cat.updateCatName(cat_exh, 2, "Whilly");
+		Cat.updateCatOwnersName(cat_exh, 3, Levina, "Mann", "Natasha");
+		Cat.updateCatAddress(cat_exh, 2, "SPb");
+		Cat.updateCatFood(cat_exh, 1, "Kittycat");
+		// Cat.writeUsingInputStream();
+		// Cat.updateCatId(cat_exh, 3, 5);
+		Cat.deletecat(cat_exh, 1);
+
+		for (Map.Entry<Integer, Cat> cat : cat_exh.entrySet()) {
+			cat.getValue().printcatinfo();
+		}
+	}
+
+}
